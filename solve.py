a8bbef5462c4cba1a1543dada565fcb928d050ab
Michael Lewandowski <milewgit@gmail.com>
2014-06-10 08:55:02 -0400

    Remove non-object code.

--- solve.py
+++ solve.py
@@ -92,21 +92,14 @@ class Occupied:
 
 
 num_nodes = 4
-num_variables = num_nodes * 2
 max_domain = 6
-domain = [1,2,3,4,5]
-universe = [domain] * num_variables
-universe[0] = [1]   # first node must be at (x,y) = (left, middle)
-universe[1] = [3]
-candidate_solution = [None] * num_variables
 all_solutions = []
-occupied = [[0 for x in xrange(max_domain+1)] for y in xrange(max_domain+1)] 
 scale = 20
 
 
 # node coordinates
 node_coordinates = [ Coordinate(1,5,1,5) for i in xrange(num_nodes-1) ]
-node_coordinates.insert(0, Coordinate(1,1,3,3))
+node_coordinates.insert(0, Coordinate(1,1,3,3))   # first node locked at coordinate (1,3)
   
   
 # occupations
@@ -124,24 +117,6 @@ connections = [
   Connection(nodes[2], nodes[3]),
   Connection(nodes[0], nodes[2]),
 ]
-
-
-def find_all_solutions(i):
-  if i == num_variables:
-    save_candidate_solution(candidate_solution)
-  else:
-    for value in universe[i]:
-      candidate_solution[i] = value
-      result = True
-      if (i % 2) == 1:
-        occupied[ candidate_solution[i-1] ][ candidate_solution[i] ] += 1
-        if occupied[ candidate_solution[i-1] ][ candidate_solution[i] ] > 1:
-          result = False
-      if result:
-        find_all_solutions(i+1)
-      if (i % 2) == 1:
-        occupied[ candidate_solution[i-1] ][ candidate_solution[i] ] -= 1
-      candidate_solution[i] = None
 
 
 def find_all_solutions2(i):
@@ -201,12 +176,13 @@ def find_nice_solutions(solutions, nice_function):
     elif nice == best:
       nice_solutions.append(solution)
   return nice_solutions
-
-
-def print_solutions(solutions):
+  
+  
+def print_solutions2(solutions):
+  global solution
   output_start()
-  for solution in solutions: 
-    output(solution)
+  for solution in solutions:
+    output2()
   output_end()
 
 
@@ -218,56 +194,11 @@ def output_start():
     """ )
   
 
-def output(solution):
-  
-  # start svg tag
-  print( """\
-    <svg width='%d' height='%d' viewBox='-1,-1,%d,%d' style='margin:1em;'>
-    """ % ((max_domain + 1) * scale, (max_domain + 1) * scale, (max_domain + 1) * scale, (max_domain + 1) * scale) )
-    
-  # horizontal grid lines
-  for y in xrange(0, max_domain + 1):
-    print( """
-      <line x1='0' y1='%d' x2='%d' y2='%d' stroke='#eee' stroke-width='1' shape-rendering='crispEdges' />
-      """ % (y * scale, max_domain * scale, y * scale) )
-      
-  # vertical grid lines
-  for x in xrange(0, max_domain + 1):
-    print( """
-      <line x1='%d' y1='0' x2='%d' y2='%d' stroke='#eee' stroke-width='1' shape-rendering='crispEdges' />
-      """ % (x * scale, x * scale, max_domain * scale) )
-  
-  # connections
-  for connection in connections:
-    x1, y1 = solution[connection.node1] * scale, solution[connection.node1 + 1] * scale
-    x2, y2 = solution[connection.node2] * scale, solution[connection.node2 + 1] * scale
-    print( "<line x1='%d' y1='%d' x2='%d' y2='%d' stroke='black' stroke-width='1' shape-rendering='crispEdges' />" % (x1, y1, x2, y2) )
-    
-  # nodes
-  for i in range(0, num_variables, 2):
-    x, y = solution[i] * scale, solution[i+1] * scale
-    print( "<rect x='%d' y='%d' width='15' height='15' stroke='black' stroke-width='1' shape-rendering='crispEdges' fill='yellow' />" % (x-8, y-8) )
-    print( "<text x='%d' y='%d' font-family=Monospace style='text-anchor: middle;'>%d</text>" % (x, y+3, (i / 2)) )
-    
-  # end svg tag
-  print( """\
-    </svg>
-    """ )
-  
-
 def output_end():
   print( """\
     </body>
   </html>
   """ )
-  
-  
-def print_solutions2(solutions):
-  global solution
-  output_start()
-  for solution in solutions:
-    output2()
-  output_end()
   
   
 def output2():
