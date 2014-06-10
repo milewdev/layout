edd8cd6f42b7231546ae2e33dd750654dea9ccf0
Michael Lewandowski <milewgit@gmail.com>
2014-06-10 12:41:19 -0400

    Remove old lab and experiment files.

--- lab.py
+++
@@ -1,7 +0,0 @@
-from constraint import *
-problem = Problem()
-problem.addVariables(["a", "b"], [1, 2])
-def func(a, b):
-  return b > a
-problem.addConstraint(func, ["a", "b"])
-print(problem.getSolution())
